11e464901d2584a77bdae98a0df7ec98ec867e4d
dwl5 <ssop6403@gmail.com>
2021-07-04 02:29:14 +0900

    refactor: 생성, 수정시간 Auditing 적용

--- src/main/java/qna/common/UpdatableEntity.java
+++ src/main/java/qna/common/UpdatableEntity.java
@@ -1,12 +1,20 @@
 package qna.common;
 
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import javax.persistence.Column;
+import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
+@EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 public abstract class UpdatableEntity {
     @Column(nullable = false)
-    private LocalDateTime createdAt = LocalDateTime.now();
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @LastModifiedBy
     private LocalDateTime updatedAt;
 }
